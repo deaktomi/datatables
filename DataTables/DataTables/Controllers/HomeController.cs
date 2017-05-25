dcc4a1ab7834f2567053b1b865da5f9a45abce8e
Tamas Deak <tamas.deak.93@gmail.com>
2017-05-25 12:15:48 +0200

    Added paging, sorting, filtering (not really by column)

--- DataTables/DataTables/Controllers/HomeController.cs
+++ DataTables/DataTables/Controllers/HomeController.cs
@@ -19,23 +19,48 @@ namespace DataTables.Controllers
         {
             var allCompanies = DataRepository.GetCompanies();
             IEnumerable<Company> filteredCompanies;
+            //Check whether the companies should be filtered by keyword
             if (!string.IsNullOrEmpty(param.sSearch))
             {
+                //Used if particulare columns are filtered 
+                var nameFilter = Convert.ToString(Request["sSearch_1"]);
+                var addressFilter = Convert.ToString(Request["sSearch_2"]);
+                var townFilter = Convert.ToString(Request["sSearch_3"]);
+
+                //Optionally check whether the columns are searchable at all 
+                var isNameSearchable = Convert.ToBoolean(Request["bSearchable_1"]);
+                var isAddressSearchable = Convert.ToBoolean(Request["bSearchable_2"]);
+                var isTownSearchable = Convert.ToBoolean(Request["bSearchable_3"]);
+
                 filteredCompanies = DataRepository.GetCompanies()
-                    .Where(c => c.Name.Contains(param.sSearch)
+                    .Where(c => isNameSearchable && c.Name.ToLower().Contains(param.sSearch.ToLower())
                                 ||
-                                c.Address.Contains(param.sSearch)
+                                isAddressSearchable && c.Address.ToLower().Contains(param.sSearch.ToLower())
                                 ||
-                                c.Town.Contains(param.sSearch));
+                                isTownSearchable && c.Town.ToLower().Contains(param.sSearch.ToLower()));
             }
             else
             {
                 filteredCompanies = allCompanies;
             }
 
-            var displayedCompanies = filteredCompanies;
-            var result = from c in displayedCompanies
-                select new[] { Convert.ToString(c.ID), c.Name, c.Address, c.Town };
+            var isNameSortable = Convert.ToBoolean(Request["bSortable_1"]);
+            var isAddressSortable = Convert.ToBoolean(Request["bSortable_2"]);
+            var isTownSortable = Convert.ToBoolean(Request["bSortable_3"]);
+            var sortColumnIndex = Convert.ToInt32(Request["iSortCol_0"]);
+            Func<Company, string> orderingFunction = (c => sortColumnIndex == 1 && isNameSortable ? c.Name :
+                sortColumnIndex == 2 && isAddressSortable ? c.Address :
+                    sortColumnIndex == 3 && isTownSortable ? c.Town :
+                        "");
+
+            var sortDirection = Request["sSortDir_0"]; // asc or desc
+            if (sortDirection == "asc")
+                filteredCompanies = filteredCompanies.OrderBy(orderingFunction);
+            else
+                filteredCompanies = filteredCompanies.OrderByDescending(orderingFunction);
+
+            var displayedCompanies = filteredCompanies.Skip(param.iDisplayStart).Take(param.iDisplayLength);
+            var result = from c in displayedCompanies select new[] { Convert.ToString(c.ID), c.Name, c.Address, c.Town };
             return Json(new
                 {
                     sEcho = param.sEcho,
