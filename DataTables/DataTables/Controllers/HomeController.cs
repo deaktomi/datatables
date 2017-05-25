79f73e4c4f29ef222e69f18f832cc427cb77cec6
Tamas Deak <tamas.deak.93@gmail.com>
2017-05-25 12:02:40 +0200

    Searching works

--- DataTables/DataTables/Controllers/HomeController.cs
+++ DataTables/DataTables/Controllers/HomeController.cs
@@ -18,15 +18,29 @@ namespace DataTables.Controllers
         public ActionResult AjaxHandler(JQueryDataTableParamModel param)
         {
             var allCompanies = DataRepository.GetCompanies();
+            IEnumerable<Company> filteredCompanies;
+            if (!string.IsNullOrEmpty(param.sSearch))
+            {
+                filteredCompanies = DataRepository.GetCompanies()
+                    .Where(c => c.Name.Contains(param.sSearch)
+                                ||
+                                c.Address.Contains(param.sSearch)
+                                ||
+                                c.Town.Contains(param.sSearch));
+            }
+            else
+            {
+                filteredCompanies = allCompanies;
+            }
 
-            var result = from c in allCompanies
-                select new[] { c.ID.ToString(), c.Name, c.Address, c.Town };
-
+            var displayedCompanies = filteredCompanies;
+            var result = from c in displayedCompanies
+                select new[] { Convert.ToString(c.ID), c.Name, c.Address, c.Town };
             return Json(new
                 {
                     sEcho = param.sEcho,
                     iTotalRecords = allCompanies.Count(),
-                    iTotalDisplayRecords = allCompanies.Count(),
+                    iTotalDisplayRecords = filteredCompanies.Count(),
                     aaData = result
                 },
                 JsonRequestBehavior.AllowGet);
